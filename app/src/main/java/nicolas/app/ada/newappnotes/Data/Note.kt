d7b0692ec08d40ffcb32a65452ebe4a1507c0208
nicolas <niruizbo94@gmail.com>
2024-02-26 15:04:52 -0500

    se crea la base de datos  y la entidad de datos

--- app/src/main/java/nicolas/app/ada/newappnotes/Data/Note.kt
+++ app/src/main/java/nicolas/app/ada/newappnotes/Data/Note.kt
@@ -1,5 +1,14 @@
 package nicolas.app.ada.newappnotes.Data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Note (
-val uid : Int
+    @PrimaryKey val uid : Int?,
+    @ColumnInfo val color : String?,
+    @ColumnInfo val text : String?,
+    @ColumnInfo val date : Int?
+
 )
