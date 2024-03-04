6cf8abbc1d967e38582cb9319555082784cfda5d
nicolas <niruizbo94@gmail.com>
2024-03-04 11:16:43 -0500

    se implementa el Adapter.kt y el contenedor de vistas

--- app/src/main/java/nicolas/app/ada/newappnotes/UI/MainActivity.kt
+++ app/src/main/java/nicolas/app/ada/newappnotes/UI/MainActivity.kt
@@ -1,21 +1,25 @@
-package nicolas.app.ada.newappnotes
+package nicolas.app.ada.newappnotes.UI
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import nicolas.app.ada.newappnotes.Data.AppDataBase
 import nicolas.app.ada.newappnotes.Data.Note
+import nicolas.app.ada.newappnotes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeDatabase()
     }
-    val db =Room.databaseBuilder(
-        applicationContext,
 
-        AppDataBase::class.java, "database-name"
-    ).build()
-    val noteDao = db.noteDao()
-    val notes : List<Note> = noteDao.getAll()
+    fun initializeDatabase() {
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDataBase::class.java, "database-name"
+        ).build()
+        val noteDao = db.noteDao()
+        val notes: List<Note> = noteDao.getAll()
+    }
 }
