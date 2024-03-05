a0b6782d24b55085c6b71cb100d1996adf4a1a14
nicolas <niruizbo94@gmail.com>
2024-03-05 17:22:44 -0500

    se  hace la configuracion del reclyerView

--- app/src/main/java/nicolas/app/ada/newappnotes/UI/MainActivity.kt
+++ app/src/main/java/nicolas/app/ada/newappnotes/UI/MainActivity.kt
@@ -2,24 +2,37 @@ package nicolas.app.ada.newappnotes.UI
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import nicolas.app.ada.newappnotes.Data.AppDataBase
 import nicolas.app.ada.newappnotes.Data.Note
+import nicolas.app.ada.newappnotes.Data.NoteDao
 import nicolas.app.ada.newappnotes.R
 
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteDao: NoteDao
+    lateinit var noteAdapter: NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initializeDatabase()
+        val recyclerView : RecyclerView =findViewById(R.id.recyclerview)
+        initDataBase()
+        configureRecyclerView(recyclerView)
+
     }
 
-    fun initializeDatabase() {
+    fun configureRecyclerView(recyclerView: RecyclerView){
+        recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+        noteAdapter = NoteAdapter(arrayOf())
+    }
+    fun initDataBase(){
         val db = Room.databaseBuilder(
             applicationContext,
             AppDataBase::class.java, "database-name"
         ).build()
-        val noteDao = db.noteDao()
+         noteDao = db.noteDao()
         val notes: List<Note> = noteDao.getAll()
     }
 }
